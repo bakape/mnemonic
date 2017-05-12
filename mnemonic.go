52c8610a0d8ed79fed5e08d863035e7bb92b5ca1
bakape <scorpid33@gmail.com>
2017-05-12 14:20:50 +0300

    Optimise and refactor

--- mnemonic.go
+++ mnemonic.go
@@ -1,78 +1,62 @@
+/*Package mnemonics makes textual representations out of IP adresses
+
+Algorithm description:
+Check of the IP is valid IPv4 or IPv6
+
+Append the salt to the IP
+SHA1 the IP
+Split the SHA 1 in to 4 chunks of 5 bytes
+
+For every chunk of 5 bytes take the first 4 bytes
+
+Convert the 4 bytes to a hex representation
+Convert the hex representation to a uint32
+
+mod the uint32 by 256 and devide that by 16
+Use this result as an index for the Mnemonic start array
+Append the array index's value to the output result
+
+mod the uint32 by 16
+Use this result as an index for the Mnemonic end array
+Append the array index's value to the output result
+
+This will give you 8 appends in total the resulting array is your output
+*/
 package mnemonic
 
 import (
 	"crypto/sha1"
-	"errors"
-	"fmt"
-	"net"
-	"strconv"
+	"encoding/binary"
 )
 
-//Top nibble
-var mnemonicStarts [16]string
-
-//Bottom nibble
-var mnemonicEnds [16]string
-
-const saltLength = 40
-
-var salt string
-
-func init() {
-	mnemonicStarts = [16]string{"", "k", "s", "t", "d", "n", "h", "b", "p", "m", "f", "r", "g", "z", "l", "ch"}
-	mnemonicEnds = [16]string{"a", "i", "u", "e", "o", "a", "i", "u", "e", "o", "ya", "yi", "yu", "ye", "yo", "'"}
-	err := SetSalt("LALALALALALALALALALALALALALALALALALALALA")
-	if err != nil {
-		panic(err)
+var (
+	// Top nibble
+	mnemonicStarts = [16]string{
+		"", "k", "s", "t", "d", "n", "h", "b", "p", "m", "f", "r", "g", "z",
+		"l", "ch",
 	}
-}
 
-// InvalidIPError indicates that the passed IP address is of invalid format
-type InvalidIPError struct{ s string }
-
-func (e InvalidIPError) Error() string {
-	return "Not a valid IP address: " + e.s
-}
-
-func validIp(s string) error {
-	ip := net.ParseIP(s)
-	if ip == nil {
-		return InvalidIPError{s}
+	// Bottom nibble
+	mnemonicEnds = [16]string{
+		"a", "i", "u", "e", "o", "a", "i", "u", "e", "o", "ya", "yi", "yu",
+		"ye", "yo", "'",
 	}
-	if ip := ip.To4(); ip != nil {
-		return nil
-	}
-	if ip := ip.To16(); ip != nil {
-		return nil
-	}
-	return InvalidIPError{s}
-}
+)
 
-func Mnemonic(addr string) (string, error) {
-	err := validIp(addr)
-	if err != nil {
-		return "", err
-	}
-	salted := addr + salt
-	out := sha1.Sum([]byte(salted))
-
-	var result string
+// Mnemonic generates a cryptographically-secure human-readable IP
+// representation. IP must be a valid IPv4 or IPv6 and salt is recommended to be
+// at least 40 chars long.
+func Mnemonic(ip, salt string) string {
+	var (
+		sum    = sha1.Sum([]byte(ip + salt))
+		result = make([]byte, 0, 8)
+	)
 	for i := 0; i < 4; i++ {
-		//This takes 4 bytes instead of 5, it looks that way in the C++!
-		part := fmt.Sprintf("%x", out[i*5:((i+1)*5)-1])
-		val, err := strconv.ParseUint(part, 16, 64)
-		if err != nil {
-			return "", err
-		}
-		result += mnemonicStarts[(val%256)/16] + mnemonicEnds[val%16]
-	}
-	return result, nil
-}
+		// This takes 4 bytes instead of 5. It looks that way in the C++!
+		j := binary.BigEndian.Uint32(sum[i*5:])
 
-func SetSalt(s string) error {
-	if len([]byte(s)) < saltLength {
-		return errors.New(fmt.Sprintf("Salt length %d not the same as recommended %d\n", len([]byte(s)), saltLength))
+		result = append(result, mnemonicStarts[(j%256)/16]...)
+		result = append(result, mnemonicEnds[j%16]...)
 	}
-	salt = s
-	return nil
+	return string(result)
 }
--- mnemonics_test.go
+++ mnemonics_test.go
@@ -6,95 +6,18 @@ import (
 	"testing"
 )
 
-func init() {
-	err := SetSalt(`heewiuhfiuwhfiwuhfeiuhellorewrhwiuehrwiuhiuweh`)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func TestSaltLength(T *testing.T) {
-	salts := []struct {
-		salt     string
-		expected bool
-	}{
-		{"heewiuhfiuwhfiwuhfeiuhellorewrhwiuehrwiuhiuweh", true},
-		{"wuhfeiuhellorewrhwiuehrwiuhiuweh", false},
-	}
-	for _, v := range salts {
-		err := SetSalt(v.salt)
-		if err != nil {
-			if v.expected == false {
-			} else {
-				T.Fatal(err)
-			}
-		} else {
-			if v.expected == true {
-			} else {
-				T.Fatal("Salt should have have for length but didn't")
-			}
-		}
-	}
-
-}
-
-func TestIp(T *testing.T) {
-	testIps := []struct {
-		ip    string
-		valid bool
-	}{
-		{"Meguca", false},
-		{"257.0.0.0", false},
-		{"0.0.0.257", false},
-		{"0.0.10.0", true},
-		{"0.0.0.0", true},
-		{"255.255.255.255", true},
-		{"10.20.100", false},
-	}
-	for _, v := range testIps {
-		err := validIp(v.ip)
-		if v.valid {
-			if err != nil {
-				T.Fatal(err, v.ip)
-			}
-		} else {
-			if err == nil {
-				T.Fatal(err, v.ip)
-			}
-		}
-	}
-}
+const salt = "r088PUX0qpUjhUyZby6e4pQcDh3zzUQUpeLOy7Hb"
 
 type inputCase struct {
 	expected string
 	input    string
-	valid    bool
 }
 
 func TestMnemonic(T *testing.T) {
-	testMnemonics := struct {
-		salt  string
-		cases []inputCase
-	}{salt: "r088PUX0qpUjhUyZby6e4pQcDh3zzUQUpeLOy7Hb"}
-
-	err := SetSalt(testMnemonics.salt)
-	if err != nil {
-		panic(err)
-	}
-	testMnemonics.cases = append(testMnemonics.cases, readCases("mnemonics_out")...)
-
-	for _, test := range testMnemonics.cases {
-		res, err := Mnemonic(test.input)
-		if test.valid && err == nil {
-			if test.expected == res {
-			} else {
-				T.Fatalf("%v, %s, %v\n", test, res, err)
-			}
-		} else if !test.valid && err != nil {
-			if test.expected == res {
-			} else {
-				T.Fatalf("%v, %s, %v\n", test, res, err)
-			}
+	for _, test := range readCases("mnemonics_out") {
+		res := Mnemonic(test.input, salt)
+		if res != test.expected {
+			T.Fatalf("%v : %s\n", test, res)
 		}
 		T.Log(test)
 	}
@@ -111,11 +34,10 @@ func readCases(fileName string) (cases []inputCase) {
 		if len(fields) != 2 {
 			continue
 		}
-		err := validIp(fields[0])
-		if err != nil {
-			panic(err)
-		}
-		cases = append(cases, inputCase{expected: fields[1], input: fields[0], valid: true})
+		cases = append(cases, inputCase{
+			expected: fields[1],
+			input:    fields[0],
+		})
 	}
 	return
 }
